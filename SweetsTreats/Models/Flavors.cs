08a6ef5a46aadfb9f26d3fdd9a64294493049d46
Trevor Teigen <trevor.teigen123@gmail.com>
2019-08-16 11:13:30 -0700

    update models for database compatibility

--- SweetsTreats/Models/Flavors.cs
+++ SweetsTreats/Models/Flavors.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 
 namespace SweetsTreats.Models
 {
     public class Flavor
     {
-        public string Name {get;set;}
-        public int FlavorId {get;set;}
+        public Flavor()
+        {
+            this.Treats = new HashSet<FlavorTreat>();
+        }
+
+        public int FlavorId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<FlavorTreat> Treats { get; set; }
     }
 }
--- SweetsTreats/Models/Treats.cs
+++ SweetsTreats/Models/Treats.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 
 namespace SweetsTreats.Models
 {
     public class Treat
     {
-        public string Name {get;set;}
-        public int TreatId {get;set;}
+        public Treat()
+        {
+            this.Flavors = new HashSet<FlavorTreat>();
+        }
+
+        public int TreatId { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<FlavorTreat> Flavors { get; }
     }
 }
